2e68f0c63fd9bbbc2b8aee4dfbcc9c3b26351e80
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-18 19:20:38 +0200

    feat: game Tenzies

--- src/Die.js
+++ src/Die.js
@@ -1,16 +1,15 @@
-import React from "react"
+import ClassNames from "classnames";
+import React from "react";
 
 export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
-    }
-    return (
-        <div 
-            className="die-face" 
-            style={styles}
-            onClick={props.holdDice}
-        >
-            <h2 className="die-num">{props.value}</h2>
-        </div>
-    )
+  const Buttonstyles = ClassNames(
+    "h-12 w-12 shadow-lg rounded-xl flex justify-center items-center cursor-pointer",
+    { "bg-green-400": props.isHeld },
+    { "bg-white": !props.isHeld }
+  );
+  return (
+    <div className={Buttonstyles} onClick={props.holdDice}>
+      <h2 className="text-3xl">{props.value}</h2>
+    </div>
+  );
 }
